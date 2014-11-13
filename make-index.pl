ebe9e6881a11b88b4f9f2e0d5b412ee4909d7f7b
Norman Walsh <ndw@nwalsh.com>
2014-11-13 06:57:57 -0600

    Update make-index.pl for new layout

--- make-index.pl
+++ make-index.pl
@@ -20,46 +20,24 @@ every commit.</p>
 EOF1
 
 my @branches = ();
-opendir (DIR, "langspec");
-while (my $fn = readdir(DIR)) {
-    next if $fn =~ /^\.\.?$/;
-    next if $fn eq 'xproc20';
-    push (@branches, $fn) if -d "langspec/$fn";
+opendir(DIR, "langspec");
+while (readdir(DIR)) {
+    next if /^\.\.?$/;
+    next if ! -d "langspec/$_";
+    push (@branches, $_) unless $_ eq 'xproc20';
 }
-closedir (DIR);
+closedir(DIR);
 
-my $pubdate = pubdate("langspec", "xproc20");
-
-print <<EOF2;
-<dl>
-<dt>The “head” branch:</dt>
-<dd>
-<ul>
-<li><a href="/specification/langspec/xproc20/head">XProc 2.0: An XML Pipeline Language</a>
-specification ($pubdate)</li>
-<li><a href="/specification/langspec/xproc20/head/ns/xproc.html">Namespace document</a>
-for <code>http://www.w3.org/ns/xproc</code></li>
-<li><a href="/specification/langspec/xproc20/head/ns/xproc-step.html">Namespace document</a>
-for <code>http://www.w3.org/ns/xproc-step</code></li>
-<li><a href="/specification/langspec/xproc20/head/ns/xproc-error.html">Namespace document</a>
-for <code>http://www.w3.org/ns/xproc-error</code></li>
-</ul>
-</dd>
-EOF2
-
-foreach my $branch (sort { $a cmp $b } @branches) {
-    $pubdate = pubdate("langspec", $branch);
-    print "<dt>$branch</dt>\n";
+print "<dl>\n";
+foreach my $branch ("xproc20", sort { $a cmp $b } @branches) {
+    my $fn = "langspec/$branch/head/xproc20/index.html";
+    my $date = pubdate($fn);
+    print "<dt>The <em>$branch</em> branch, $date:</dt>\n";
     print "<dd>\n";
-    print "<ul>\n";
-    print "<li><a href=\"/specification/langspec/$branch/head\">XProc 2.0: An XML Pipeline Language</a> specification ($pubdate)</li>\n";
-    print "<li><a href=\"/specification/langspec/$branch/head/ns/xproc.html\">Namespace document</a> for <code>http://www.w3.org/ns/xproc</code></li>\n";
-    print "<li><a href=\"/specification/langspec/$branch/head/ns/xproc-step.html\">Namespace document</a> for <code>http://www.w3.org/ns/xproc-step</code></li>\n";
-    print "<li><a href=\"/specification/langspec/$branch/head/ns/xproc-error.html\">Namespace document</a> for <code>http://www.w3.org/ns/xproc-error</code></li>\n";
-    print "</ul>\n</dd>\n\n";
+    &showspec($branch);
+    print "</dd>\n";
 }
-
-print "</dl>\n\n";
+print "</dl>\n";
 
 print <<EOF3;
 <p>Remember: these documents are editor's drafts with no normative
@@ -78,8 +56,7 @@ exit 0;
 
 sub pubdate {
     my $spec = shift;
-    my $branch = shift;
-    open (F, "$spec/$branch/head/index.html") || return "date unknown";
+    open (F, $spec) || return "date unknown";
     read (F, $_, 4096);
     close (F);
     s/^.*?<h2>(.*?)<\/h2>.*$/$1/s;
@@ -87,8 +64,29 @@ sub pubdate {
     if (/\d+\s+\S+\s+\d+/) {
         $_ = $MATCH;
     } else {
-        $_ = "date unknown"
+        $_ = "";
     }
 
     return $_;
+}
+
+sub title {
+    my $spec = shift;
+    open (F, $spec) || return "date unknown";
+    read (F, $_, 4096);
+    close (F);
+    s/^.*?<div class=.title.>(.*?)<\/div>.*$/$1/s;
+    return $_;
+}
+
+sub showspec {
+    my $branch = shift;
+    print "<ul>\n";
+    foreach my $name ("xproc20", "xproc20-steps", "ns-p", "ns-c", "ns-err") {
+        my $href = "langspec/$branch/head/$name/";
+        my $fn = "$href/index.html";
+        my $title = title($fn);
+        print "<li><a href=\"$href\">$title</a></li>\n";
+    }
+    print "</ul>\n";
 }
